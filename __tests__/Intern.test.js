3d9c9369334ba279a75b7b7e16f91bec5f9089a7
Rae Alejandrino <raealejandrino@gmail.com>
2021-11-24 13:45:11 -0500

    Passed intern test, initialized Employee constructor

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,14 +3,15 @@ const Intern = require('../lib/Intern');
 describe('Intern', () => {
     it('Should create an Intern object', () => {
         //setup
-        const intern = new Intern('Dave', '10', 'dave@example.com', 'Example University');
+        const intern = new Intern('Dave', 10, 'dave@example.com', 'Example University');
 
         //execute
 
         //assertions
         expect(intern.name).toBe('Dave');
         expect(intern.id).toEqual(expect.any(Number));
-        expect(intern.email).toEqual(expect.stringContaining(player))
-    })
-})
+        expect(intern.email).toBe('dave@example.com');
+        expect(intern.school).toBe('Example University');
+    });
+});
 
