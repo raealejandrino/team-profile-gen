7de55ec7b06700b6251cee03bf2bf63d2931349f
Rae Alejandrino <raealejandrino@gmail.com>
2021-11-27 13:17:02 -0500

    Intialized testing for engineer constructor

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,12 @@
 const Engineer = require('../lib/Engineer');
 
+describe('Engineer', () => {
+    it('Should represent an object with the properties of an Engineer', () => {
+        const engineer = new Engineer('Dave', 10, 'dave@example.com', 'daveGit');
+
+        expect(engineer.name).toBe('Dave');
+        expect(engineer.id).toEqual(expect.any(Number));
+        expect(engineer.email).toBe('dave@example.com');
+        expect(engineer.github).toBe('daveGit');
+    });
+});
